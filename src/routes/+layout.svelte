24b0275a884fd12b5b441e287d3be71cc118936e
Anna Nilsson <nilsson.j.anna@gmail.com>
2023-07-14 15:49:56 +0200

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import '../app.css';
+	const tabs = ['Hem', 'Vison', 'Praktiskt'];
+</script>
+
+<header class="bg-teal-300 h-10" />
+
+<img
+	src="/img/tftsLogga.png"
+	alt="Tales from the sprawl using a purple, pink and teal dragon with a skull for a head as the s in Sprawl"
+/>
+<nav class="mx-auto max-w-prose">
+	<ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200">
+		{#each tabs as tab}
+			<li class="mr-2">
+				<a
+					href="#"
+					class="inline-block p-4 text-blue-600 bg-teal-100 rounded-t-lg dark:bg-gray-800 dark:text-blue-500"
+					>{tab}</a
+				>
+			</li>
+		{/each}
+
+		<li class="mr-2">
+			<a
+				href="#"
+				class="inline-block p-4 rounded-t-lg border-4 border-teal-400 hover:text-gray-600 hover:bg-gray-50"
+				>Dashboard</a
+			>
+		</li>
+	</ul>
+</nav>
+
+<slot />
+
+<footer class="bg-amber-400 px-3 py-4">
+	<p class="font-bold">Designed by nill</p>
+</footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<main class=" prose mx-auto border-t-4 border-b-4 border-teal-400 p-6 box-content">
+	<h1 class="text-5xl">Hem</h1>
+	<p class="">
+		Har du för lite matrix och katanas i din mainstreamfantasy? Eller för få orker och magiker i
+		dina framtidsdystopier? Misströsta inte, för nu blir det äntligen Shadowrunlajv igen! 2023 års
+		lajv går av stapeln lördag 14:e oktober i Göteborg. Anmälan öppnar snart! Enkvälls, lekfullt,
+		färgsprakande klubblajv i den Sjätte Tidsåldern. Bygg ett cyberdeck av en gammal synth! Limma på
+		dig alvöronen bakochfram och ta på dig dina snabbaste solglasögon! Vi ses i betongdjungeln! Vill
+		du veta allt som är novahot? Eller få uppdateringar från crew? Anmäl dig till vårt nyhetsbrev
+		genom att fylla i formuläret nedan!
+	</p>
+
+	<h3 class="text-3xl">Kontakt</h3>
+	<p>
+		Arrangörsgruppen nås på <a href="mailto:talesfromthesprawl@gmail.com"
+			>talesfromthesprawl@gmail.com.</a
+		>
+	</p>
+	<p>
+		Passa även på att gå med i <a href="https://www.facebook.com/groups/1813934291981275/"
+			>Facebookgruppen!</a
+		>
+	</p>
+	<p>
+		Här är även en <a href="https://drive.google.com/drive/u/0/folders/0BxSwtIpYEdVxQTNlbFVsd1JDVW8"
+			>länk</a
+		> där mailutskicken inför förra årets lajv finns, så att ni kan få en uppfattning om vad som gäller!
+	</p>
+</main>
